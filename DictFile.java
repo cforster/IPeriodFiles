821fa8917a6ad9184f533ce7526b277592f40017
Charles Forster <charlie@forsters.com>
2012-11-06 12:34:35 -0500

    DictFile for the map assignment

---
+++ DictFile.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class DictFile {
+
+    public static void main(String[] args) {
+	String  s = "O for a Muse of fire, that would ascend The brightest heaven of invention, A kingdom for a stage, princes to act";
+
+	
+	Map<String, Integer> freq = new LinkedHashMap<String, Integer>();
+
+	//go through word by word
+	     //check if its in the map already
+	          //increase the value associated with that key by one 
+	     //else
+	         //put it into the map as a key with 1
+
+	//go through every key in the map
+	     //print out the key and the value.
+
+
+	//sample code:
+	/*
+	freq.put("O", new Integer(1));
+	System.out.println(freq.get("O"));
+	*/
+
+	//BONUSES:
+	//open a file, and do this with that file
+	//
+
+
+
+    }
+}
+
