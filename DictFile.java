85cf4e92405fc9d7742e8e7ab71b8025457b98f9
C14zg <student@C02G20YYDDR0.dalton.org>
2012-11-13 11:51:34 -0500

    Zach G and Cal B Map

--- DictFile.java
+++ DictFile.java
@@ -3,10 +3,10 @@ import java.util.*;
 class DictFile {
 
     public static void main(String[] args) {
-	String  s = "O for a Muse of fire, that would ascend The brightest heaven of invention, A kingdom for a stage, princes to act";
+	String  s = "O for a Muse of fire, that would ascend The brightest heaven of inventon, A kingdom for a stage, princes to act";
 
-	
 	Map<String, Integer> freq = new LinkedHashMap<String, Integer>();
+
 
 	//go through word by word
 	     //check if its in the map already
@@ -16,13 +16,27 @@ class DictFile {
 
 	//go through every key in the map
 	     //print out the key and the value.
+    
+	String [] words = s.split(" "); 
+	for(int i=0; i<words.length;i++)
+	    {
+		if(freq.containsKey(words[i]))
+	        {
+		    freq.put(words[i], freq.get(words[i])+1);
+		}
 
+		else
+		{
+		    freq.put (words[i], new Integer(1));
+		}
 
-	//sample code:
-	/*
-	freq.put("O", new Integer(1));
-	System.out.println(freq.get("O"));
-	*/
+	    }
+
+	for(String key : freq.keySet()) {
+	    System.out.println(key + ":" + freq.get(key));
+	}
+       
+ 
 
 	//BONUSES:
 	//open a file, and do this with that file
